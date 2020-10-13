69808bd6baeaa7325f4eff9120dfda3548d1c64a
Adrian Grzemski <adrian.grzemski@gmail.com>
2020-10-13 23:09:27 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/env/bin/python3
+
+from setuptools import setup
+
+
+setup(name='dstoolkit',
+      version='0.1',
+      description='Data Science Toolkit',
+      url='',
+      author='Adrian Grzemski',
+      author_email='adrian.grzemski@gmail.com',
+      license='LGPL3.0',
+      packages=['describe'],
+      zip_safe=False)
